1fd37b94080d3827d44642ccbdcc191ad0495f8c
Sergeicoding <66889666+Sergeicoding@users.noreply.github.com>
2020-12-19 22:05:24 -0500

    Sergei's course.page.js (#3)
    https://jira.pasv.us/browse/QA7-857

---
+++ pageobjects/course.page.js
@@ -0,0 +1,13 @@
+import BasePage from './BasePage';
+
+class CoursePage extends BasePage {
+  get lesson () {
+    return $('a[href$="5cde476f21fc0e0038aaabdd"]');
+  };
+
+  openLesson () {
+    this.lesson.click();
+  }
+}
+
+module.exports = new LoginPage();
