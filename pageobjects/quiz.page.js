9e96b1b5fe07df63551eca5998db8b0786c515db
dannywhy <73038718+dannywhy@users.noreply.github.com>
2020-12-23 18:23:20 -0500

    quizPage POM QA7-846 (#5)
    https://jira.pasv.us/browse/QA7-846

---
+++ pageobjects/quiz.page.js
@@ -0,0 +1,51 @@
+import BasePage from './BasePage';
+import LoginPage from './login.page';
+
+class QuizPage extends LoginPage {
+  get startQuizButton () {
+    return $('.ant-btn');
+  }
+
+  get title () {
+    return $('.mr-2')[5];
+  }
+
+  get progress () {
+    return $('.mt-4.mb-4');
+  }
+
+  get progressBar () {
+    return $('.ant-progress-steps-outer');
+  }
+
+  get questionTitle () {
+    return $$('.mb-4')[1];
+  }
+
+  get answerCheckBox () {
+    return $$('.ant-radio-inner');
+  }
+
+  get submitButton () {
+    return $('ant-btn.ant-btn-primary');
+  }
+
+  startQuiz () {
+    this.startQuizButton.click();
+  }
+
+  getTitle () {
+    this.title.getText();
+  }
+
+  selectAnswer () {
+    this.answerCheckBox.click();
+  }
+
+  submit () {
+    this.submitButton.click();
+  }
+};
+
+module.exports = new LoginPage();
+module.exports = new QuizPage();
