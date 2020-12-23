8a9bf848d7da37ec3f1cfae75921766c70d4ead6
daniilyamkavoi <danik.2012@mail.ru>
2020-12-23 18:21:34 -0500

    quizPage POM

---
+++ pageobjects/BasePage.js
@@ -0,0 +1,13 @@
+export default class BasePage {
+  get homePageLink () {
+    return $('.site-name');
+  }
+
+  get header () {
+    return $('h1');
+  }
+
+  open (path) {
+    return browser.url(path);
+  }
+}
