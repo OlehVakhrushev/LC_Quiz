32c113d66ca35dc3d52d49cf91dab292ae5e4c38
sergeiioannisyan <sergeicoding@gmail.com>
2020-12-27 17:56:20 -0500

    lessonPage.js edited

--- pageobjects/lessonPage.js
+++ pageobjects/lessonPage.js
@@ -2,7 +2,7 @@ import BasePage from './BasePage';
 
 class LessonPage extends BasePage {
   get quizName () {
-    return $('a[href$="5cde476f21fc0e0038aaabdd/questiongroup/5db366c1b9de150038675e0f"]');
+    return $('a[href$="5db366c1b9de150038675e0f"]');
   };
 
   openQuiz () {
