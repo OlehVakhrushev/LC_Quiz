587311c8dc549e0f67649751f82421182cd88274
dannywhy <73038718+dannywhy@users.noreply.github.com>
2021-01-12 21:27:24 -0500

    goToQuiz function, reformat POM (#15)
    * goToQuiz function
    reformat POM
    
    * fixed imports

--- pageobjects/course.page.js
+++ pageobjects/course.page.js
@@ -1,29 +1,31 @@
 import BasePage from './BasePage';
 
 class CoursePage extends BasePage {
-  get lessonString () {
-    return $('a[href$="5d2eb6620f1c8500385e7064"]');
-  }
-
+  // get lessonString () {
+  //   return $('a[href$="5d2eb6620f1c8500385e7064"]');
+  // }
+  // get lesson () {
+  //   return $('a[href$="5cde476f21fc0e0038aaabdd"]');
+  // }
+  //
+  // get lessonSyntax () {
+  //   return $('a[href$="5cde476f21fc0e0038aaabdd"]');
+  // };
   get lesson () {
-    return $('a[href$="5cde476f21fc0e0038aaabdd"]');
+    return $$('.edit.ml-2');
   }
 
-  get lessonSyntax () {
-    return $('a[href$="5cde476f21fc0e0038aaabdd"]');
-  };
+  // openLessonString () {
+  //   this.lessonString.click();
+  // }
 
-  openLessonString () {
-    this.lessonString.click();
+  openLesson (i) {
+    this.lesson[i].click();
   }
 
-  openLesson () {
-    this.lesson.click();
-  }
-
-  openLessonSyntax () {
-    this.lessonSyntax.click();
-  }
+  // openLessonSyntax () {
+  //   this.lessonSyntax.click();
+  // }
 }
 
 module.exports = new CoursePage();
