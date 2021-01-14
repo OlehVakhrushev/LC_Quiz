a81da79c73282db8beab09b50b99a86f6418f7d3
annaluchenkova <LL565340@gmail.com>
2021-01-13 22:10:00 -0500

    Quiz_Present

--- test/specs/smoke/testPresentQuiz.js
+++ test/specs/smoke/testPresentQuiz.js
@@ -46,40 +46,39 @@ describe('Modul Quiz is Present in JavaScript Practice', function () {
     expect(QuizPresentPage.modulQuiz).toBeExisting();
   });
 
-  // it('Should present Header Quiz for Admin', function () {
-  //   LoginPage.open();
-  //   LoginPage.login(user.admin.email, user.admin.password);
-  //   TopMenu.openCourses();
-  //   CoursesListPage.chooseCourse();
-  //   CoursePage.openLesson();
-  //   expect(QuizPresentPage.headerQuiz.getAttribute('innerText')).to.be.equal(presentQuiz.quizHeader);
-  // });
-  //
-  // it('Should present Header Quiz for Student', function () {
-  //   LoginPage.open();
-  //   LoginPage.login(user.student.email, user.student.password);
-  //   CoursesPage.openCourse();
-  //   CoursesPage.chooseCourse();
-  //   CoursePage.openLesson();
-  //   expect(QuizPresentPage.headerQuiz.getAttribute('innerText')).to.be.equal(presentQuiz.quizHeader);
-  // });
-  //
-  // it('Should present Header Quiz for Learner', function () {
-  //   LoginPage.open();
-  //   LoginPage.login(user.learner.email, user.learner.password);
-  //   CoursesPage.openCourse();
-  //   CoursesPage.chooseCourse();
-  //   CoursePage.openLesson();
-  //   expect(QuizPresentPage.headerQuiz.getAttribute('innerText')).to.be.equal(presentQuiz.quizHeader);
-  // });
-  //
-  // it('Should present Header Quiz for Teacher', function () {
-  //   LoginPage.open();
-  //   LoginPage.login(user.teacher.email, user.teacher.password);
-  //   CoursesPage.openCourse();
-  //   CoursesPage.chooseCourse();
-  //   CoursePage.openLesson();
-  //   expect(QuizPresentPage.headerQuiz.getAttribute('innerText')).to.be.equal(presentQuiz.quizHeader);
-  // });
+  it('Should present Header Quiz for Admin', function () {
+    LoginPage.open();
+    LoginPage.login(user.admin.email, user.admin.password);
+    TopMenu.openCourses();
+    CoursesListPage.chooseCourse();
+    CoursePage.openLesson();
+    expect(QuizPresentPage.headerQuiz.getAttribute('innerText')).to.be.equal(presentQuiz.quizHeader);
+  });
 
+  it('Should present Header Quiz for Student', function () {
+    LoginPage.open();
+    LoginPage.login(user.student.email, user.student.password);
+    CoursesPage.openCourse();
+    CoursesPage.chooseCourse();
+    CoursePage.openLesson();
+    expect(QuizPresentPage.headerQuiz.getAttribute('innerText')).to.be.equal(presentQuiz.quizHeader);
+  });
+
+  it('Should present Header Quiz for Learner', function () {
+    LoginPage.open();
+    LoginPage.login(user.learner.email, user.learner.password);
+    CoursesPage.openCourse();
+    CoursesPage.chooseCourse();
+    CoursePage.openLesson();
+    expect(QuizPresentPage.headerQuiz.getAttribute('innerText')).to.be.equal(presentQuiz.quizHeader);
+  });
+
+  it('Should present Header Quiz for Teacher', function () {
+    LoginPage.open();
+    LoginPage.login(user.teacher.email, user.teacher.password);
+    CoursesPage.openCourse();
+    CoursesPage.chooseCourse();
+    CoursePage.openLesson();
+    expect(QuizPresentPage.headerQuiz.getAttribute('innerText')).to.be.equal(presentQuiz.quizHeader);
+  });
 });
